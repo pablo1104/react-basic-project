c40df57fa89ac99345634ad022a0c8640ee42591
Aura Alba <amalbas@unal.edu.co>
2018-05-07 21:45:57 -0500

    Project using react, readux, and redux-saga

---
+++ src/store/actions/index.js
@@ -0,0 +1,29 @@
+export {
+    addIngredient,
+    removeIngredient,
+    initIngredients,
+    setIngredients,
+    fetchIngredientsFailed
+} from './burgerBuilder';
+export {
+    purchaseBurger,
+    purchaseInit,
+    fetchOrders,
+    purchaseBurgerStart,
+    purchaseBurgerSuccess,
+    purchaseBurgerFail,
+    fetchOrdersStart,
+    fetchOrdersFail,
+    fetchOrdersSuccess
+} from './order';
+export {
+    auth,
+    logout,
+    setAuthRedirectPath,
+    authCheckState,
+    logoutSuceed,
+    authStart,
+    authSuccess,
+    checkAuthTimeout,
+    authFail
+} from './auth';
---
+++ src/store/sagas/burgerBuilder.js
@@ -0,0 +1,13 @@
+import {put} from 'redux-saga/effects';
+import * as actions from '../actions'
+
+import axios from "../../axios-orders";
+
+export function* initIngredientsSaga(action) {
+    try {
+        const response = yield axios.get('https://react-my-burger-20083.firebaseio.com/ingredients.json')
+        yield put(actions.setIngredients(response.data));
+    } catch (error) {
+        yield put(actions.fetchIngredientsFailed());
+    }
+}
